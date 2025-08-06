2fa24fa80fa81c3ec163d714b616918e24efaac6
Rcuhi <rappinngu2006@gmail.com>
2025-08-06 23:37:37 +0900

    Update Workspace.tsx

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -1,25 +1,21 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import React, { useEffect, useState } from "react";
-import { Parameter } from "../lib/openSCAD/parseParameter";
+import type { Parameter } from "../lib/openSCAD/parseParameter";
 import parseOpenScadParameters from "../lib/openSCAD/parseParameter";
 import Buttons from "./Workspace/Buttons";
 import CodeEditor from "./Workspace/CodeEditor";
 import Console from "./Workspace/Console";
 import Customizer from "./Workspace/Customizer";
 import FileSystem from "./Workspace/FileSystem";
+import Legend from "./Workspace/Legend";
 import Preview from "./Workspace/Preview";
 import Sidebar from "./Workspace/Sidebar";
+import { useFileSystemProvider } from "./providers/FileSystemProvider";
 import { useOpenSCADProvider } from "./providers/OpenscadWorkerProvider";
 import { useWorkspaceProvider } from "./providers/WorkspaceProvider";
-import { useFileSystemProvider } from "./providers/FileSystemProvider";
-import Legend from "./Workspace/Legend";
 
-export type EditorMode =
-  | "legend"
-  | "editor"
-  | "customizer"
-  | "file";
+export type EditorMode = "legend" | "editor" | "customizer" | "file";
 
 // Define the interface for legend item
 export interface LegendItem {
@@ -39,7 +35,7 @@ export default function Workspace() {
   const [parameters, setParameters] = useState<Parameter[]>([]);
   const [legendItems, setLegendItems] = useState<LegendItem[]>([
     { id: "0", main: "A", shift: "+", fn: "@", bump: false, center: false },
-    { id: "1", main: "B", shift: "-", fn: "&", bump: false, center: false }
+    { id: "1", main: "B", shift: "-", fn: "&", bump: false, center: false },
   ]);
 
   // Whenever the code changes, attempt to parse the parameters
@@ -52,14 +48,14 @@ export default function Workspace() {
     // Add old values to new params
     if (parameters.length) {
       // REFACTOR: rework to use a map instead of two loops
-      newParams.forEach((newParam) => {
+      for (const newParam of newParams) {
         const oldParam = parameters.find(
-          (param) => param.name === newParam.name
+          (param) => param.name === newParam.name,
         );
         if (oldParam) {
           newParam.value = oldParam.value;
         }
-      });
+      }
     }
     setParameters(newParams);
 
@@ -75,14 +71,16 @@ export default function Workspace() {
 
   // Hydrate font options
   useEffect(() => {
-    parameters.filter((p) => p.isFont).forEach((p) => {
-      p.options = files
-        .filter((f) => f.type === "font")
-        .map((f) => {
-          const nameWithoutExt = f.name.replace(/\.[^/.]+$/, "");
-          return { value: nameWithoutExt, label: nameWithoutExt };
-        });
-    });
+    for (const p of parameters) {
+      if (p.isFont) {
+        p.options = files
+          .filter((f) => f.type === "font")
+          .map((f) => {
+            const nameWithoutExt = f.name.replace(/\.[^/.]+$/, "");
+            return { value: nameWithoutExt, label: nameWithoutExt };
+          });
+      }
+    }
   }, [files, parameters]);
 
   return (
@@ -119,7 +117,11 @@ export default function Workspace() {
             borderColor: "#ccc",
           }}
         >
-          <Buttons code={code} parameters={parameters} legendItems={legendItems} />
+          <Buttons
+            code={code}
+            parameters={parameters}
+            legendItems={legendItems}
+          />
         </Grid>
         <Grid
           size={{ xs: 8 }}
